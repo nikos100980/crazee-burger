c7e3c202adb5e38c7bf4f772fde7601a91908b3c
LOPEZ Nicolas <nikos100980@free.fr>
2023-02-03 01:49:01 +0100

    ♻️ F04: ORDER>layout>navbar>add left-side and right-side

--- src/components/pages/order/Navbar.jsx
+++ src/components/pages/order/Navbar.jsx
@@ -1,37 +1,37 @@
 import React from "react";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-export default function Navbar({username}) {
-  
+export default function Navbar({ username }) {
   return (
     <NavbarStyled>
-      Navbar
-      <h1>Bonjour {username}</h1>
-      <br />
-      <Link to="/">
-        <button>Deconnexion</button>
-      </Link>
-      <div className="icon-profil">icon</div>
+      <div className="left-side">Left</div>
+      <div className="right-side">
+        Right
+        <h1>Hey, {username}</h1>
+        <br />
+        <Link to="/">
+          <button>Deconnexion</button>
+        </Link>
+        <div className="icon-profil">icon</div>
+      </div>
     </NavbarStyled>
   );
-
-  
 }
 
 const NavbarStyled = styled.nav`
-background: #3919f0;
-    display: flex;
-    flex-direction: column;
+  background: #3919f0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 10vh;
+  /* border-radius: 15px 15px 0px 0px; */
 
-    height: 10vh;
-    /* border-radius: 15px 15px 0px 0px; */
-    
-`
+  .left-side {
+    background: pink;
+  }
+  .right-side {
+    background: purple;
+  }
 
-
-
-
-
-    
-  
+`;
