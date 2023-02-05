c568d5a3d1a79b61da742d13669439daf35ae92e
LOPEZ Nicolas <nikos100980@free.fr>
2023-02-05 01:08:35 +0100

    🚧 F05: ORDER>MAIN> Add layout

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -3,16 +3,29 @@ import styled from "styled-components";
 import { theme } from "../../../theme";
 
 export default function Main() {
-  return <MainStyled>Main</MainStyled>;
+  return (
+    <MainStyled>
+      <div className="basket-container">basket</div>
+      <div className="menu-container">menu</div>
+    </MainStyled>
+  );
 }
 
 const MainStyled = styled.main`
+  background: ${theme.colors.background_white};
+  box-shadow: ${theme.shadows.strong};
+  border-bottom-left-radius: ${theme.borderRadius.extraRound};
+  border-bottom-right-radius: ${theme.borderRadius.extraRound};
+  display: flex;
+  flex-direction: row;
+  flex: 1;
 
-      background: ${theme.colors.background_white};
-      box-shadow: ${theme.shadows.strong};
-      border-bottom-left-radius: ${theme.borderRadius.extraRound};
-      border-bottom-right-radius: ${theme.borderRadius.extraRound};
-      flex: 1;
-    
-
-`
+  .basket-container {
+    background: purple;
+    flex-basis: 25%;
+  }
+  .menu-container {
+    background: green;
+    flex-basis: 75%;
+  }
+`;
