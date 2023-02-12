be6f7a73ecfba7edbde0273b1f7d4c10bce49ea2
LOPEZ Nicolas <nikos100980@free.fr>
2023-02-12 17:59:15 +0100

    ✨ F06: ORDER>NAVBAR> Create ToggleButton.jsx

--- src/components/pages/order/Navbar/NavbarRightSide.jsx
+++ src/components/pages/order/Navbar/NavbarRightSide.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-
+import { ToastContainer } from "react-toastify";
 import styled from "styled-components";
+import { theme } from "../../../../theme";
+import ToggleButton from "../../reusable-ui/ToggleButton";
 import Profile from "./Profile";
 
 export default function NavbarRightSide({ username, Icon }) {
   return (
     <NavbarRightSideStyled className="right-side">
+      <ToastContainer className="toaster" bodyClassName="body-toast" />
+      <ToggleButton />
       <Profile Icon={Icon} username={username} />
     </NavbarRightSideStyled>
   );
@@ -15,4 +19,22 @@ const NavbarRightSideStyled = styled.div`
   display: flex;
   align-items: center;
   padding-right: 50px;
+
+  .toaster {
+    max-width: 300px;
+  }
+
+  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
+    background: ${theme.colors.background_dark};
+  }
+
+  .body-toast {
+    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
+      margin-right: 20px;
+      margin-left: 5px;
+    }
+    div {
+      line-height: 1.3em;
+    }
+  }
 `;
